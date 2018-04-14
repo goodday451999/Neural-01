ddb915878572624ec5e145e94e84f4c546d968c9
Nilotpal Pramanik <33095476+goodday451999@users.noreply.github.com>
2018-04-15 00:08:35 +0530

    Small changes in Code

--- Neural_01.py
+++ Neural_01.py
@@ -1,15 +1,12 @@
-
 from math import exp
-
+#import numpy
 import numpy as np
-
-
 
 def nonlin(x, deriv=False):
     if (deriv == True):
         return x * (1 - x)
-
-    return 1 / (1 + np, exp(-x))
+    else:
+        return 1 / (1 + np, exp(-x))
 
 
 # Input Data
